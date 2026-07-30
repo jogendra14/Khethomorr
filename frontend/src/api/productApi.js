5a946fc5ce384e060c1aa6519b74bc7e97dd437c
jogendra14 <jogendrabanna46@gmail.com>
2026-07-30 11:09:31 +0530

    up

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -1,13 +1,13 @@
 import API from "./axios";
 
 export const getProduct = async () => {
-  const { data } = await API.get("/products");
+  const { data } = await API.get("/api/products");
   return data;
 };
 
 //Add Product API server ko data send krna 
 export const addProduct = async (formData) => {
-  const response = await API.post("/products", formData, {
+  const response = await API.post("/api/products", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
       Authorization: `Bearer ${localStorage.getItem("token")}`,
