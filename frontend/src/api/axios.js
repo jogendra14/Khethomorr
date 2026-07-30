7198ffc233a0fc4831e6d05ae710a315f927d862
jogendra14 <jogendrabanna46@gmail.com>
2026-07-30 09:58:01 +0530

    up

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://khethomorr.onrender.com",
+  baseURL: "https://khethomorr.onrender.com/api",
   withCredentials: true,
 });
 
