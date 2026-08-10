02d2feeba98a0058e198bcd5bd41d0f9ee2c23a1
jogendra14 <jogendrabanna46@gmail.com>
2026-08-10 13:45:18 +0530

    login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -39,9 +39,7 @@ const sendTokenResponse = (user, statusCode, res, message = 'Success') => {
   user.refreshToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
   user.refreshTokenExpire = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
   user.lastLogin = new Date();
-
-
-  await user.save({ validateBeforeSave: false });
+  user.save({ validateBeforeSave: false });
 
   // Remove password from output
   user.password = undefined;
